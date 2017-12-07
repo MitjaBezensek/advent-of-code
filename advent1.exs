ff1d7b0305626005c2f230261494502576b285f9
Mitja Bezenšek <mitja.bezensek@gmail.com>
2017-12-07 22:50:12 +0100

    Solve advent of code day 3.

---
+++ advent1.exs
@@ -0,0 +1,18 @@
+defmodule Advent1 do
+    def sum(input, offset) do
+        first_part = Enum.take(input, offset)
+        second_part = Enum.drop(input, offset)
+        shifted_input = second_part ++ first_part
+        sum(input, shifted_input, 0)
+    end
+
+    defp sum([], _, sum), do: sum
+    defp sum([a | rest], [a | rest2], sum), do: sum(rest, rest2, sum + a)
+    defp sum([_ | rest], [_ | rest2], sum), do: sum(rest, rest2, sum)
+end
+
+{:ok, input} = File.read("input1")
+input = input |> String.replace("\n", "") |> String.codepoints() |> Enum.map(&String.to_integer/1)
+input |> Advent1.sum(1) |> IO.inspect
+half_length = round(length(input) / 2)
+input |> Advent1.sum(half_length) |> IO.inspect
---
+++ advent3.exs
@@ -0,0 +1,65 @@
+defmodule Advent3 do
+    def generate(lastNum, next_element_fn), do: generate({1, 0, 0}, %{{0, 0} => 1}, 0, 0, 0, 0, lastNum, "right", next_element_fn) 
+
+    def generate({num, x, y}, _, _, _, _, _, lastNum, _, _) when num >= lastNum do
+        IO.inspect(abs(x) + abs(y)) 
+        IO.inspect(num)
+    end
+
+    def generate({num, x, y}, map, top, right, bottom, left, lastNum, "right", next_element_fn) do
+        nextValue = next_element_fn.({num, x + 1, y}, map)
+        nextElement = {nextValue, x + 1, y}
+        map = Map.put(map, {x + 1, y}, nextValue)
+        if(x + 1 > right) do
+            generate(nextElement, map, top, right + 1, bottom, left, lastNum, "up", next_element_fn)
+        else
+            generate(nextElement, map, top, right, bottom, left, lastNum, "right", next_element_fn)
+        end
+    end
+
+    def generate({num, x, y}, map, top, right, bottom, left, lastNum, "up", next_element_fn) do
+        nextValue = next_element_fn.({num, x, y + 1}, map)
+        nextElement = {nextValue, x, y + 1}
+        map = Map.put(map, {x, y + 1}, nextValue)
+        if(y + 1 > top) do
+            generate(nextElement, map, top + 1, right, bottom, left, lastNum, "left", next_element_fn)
+        else
+            generate(nextElement, map, top, right, bottom, left, lastNum, "up", next_element_fn)
+        end
+    end
+
+    def generate({num, x, y}, map, top, right, bottom, left, lastNum, "left", next_element_fn) do
+        nextValue = next_element_fn.({num, x - 1, y}, map)
+        nextElement = {nextValue, x - 1, y}
+        map = Map.put(map, {x - 1, y}, nextValue)
+        if(x - 1 < left) do
+            generate(nextElement, map, top, right, bottom, left - 1, lastNum, "down", next_element_fn)
+        else
+            generate(nextElement, map, top, right, bottom, left, lastNum, "left", next_element_fn)
+        end
+    end
+
+    def generate({num, x, y}, map, top, right, bottom, left, lastNum, "down", next_element_fn) do
+        nextValue = next_element_fn.({num, x, y - 1}, map)
+        nextElement = {nextValue, x, y - 1}
+        map = Map.put(map, {x, y - 1}, nextValue)
+        if(y - 1 < bottom) do
+            generate(nextElement, map, top, right, bottom - 1, left, lastNum, "right", next_element_fn)
+        else
+            generate(nextElement, map, top, right, bottom, left, lastNum, "down", next_element_fn)
+        end
+    end
+
+    def part1_next_element({num, _, _}, _), do: num + 1
+
+    def part2_next_element({num, x, y}, map) do
+        Enum.sum(
+            for x_offset <- -1..1, y_offset <- -1..1, {:ok, value} <- [Map.fetch(map, {x + x_offset, y + y_offset})] do 
+                value
+            end
+        )
+    end
+end
+
+Advent3.generate(368078, &Advent3.part1_next_element/2)
+Advent3.generate(368078, &Advent3.part2_next_element/2)
