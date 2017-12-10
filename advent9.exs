e7d6045625bab8565279ba8a90a6c4c854089f01
Mitja Bezenšek <mitja.bezensek@gmail.com>
2017-12-10 22:36:39 +0100

    Improve day 9.

--- advent9.exs
+++ advent9.exs
@@ -9,8 +9,7 @@ defmodule Advent9 do
                 |> String.codepoints
 
         removed_garbage = no_exclamations |> Enum.reduce(%{garbage_started: false, result: [], garbage_count: 0}, &remove_garbage/2)
-        count_groups(removed_garbage.result).sum
-            |> IO.inspect
+        IO.inspect(count_groups(removed_garbage.result).sum)
         IO.inspect(Map.fetch!(removed_garbage, :garbage_count))
     end
 
