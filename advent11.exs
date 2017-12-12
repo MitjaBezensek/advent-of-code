68834a92bc386593a7503cddff5431cd06d531e6
Mitja Bezenšek <mitja.bezensek@gmail.com>
2017-12-12 09:07:14 +0100

    Generalize the algorithm.

--- advent11.exs
+++ advent11.exs
@@ -5,10 +5,27 @@ defmodule Advent11 do
                 |> Enum.group_by(fn v -> v end)
                 |> Enum.map(fn {el, list} -> %{String.to_atom(el) => Enum.count(list)} end)
                 |> Enum.reduce(%{}, fn (el, acc) -> Enum.into(el, acc) end)
-        north = moves.n - moves.s
-        ne = moves.ne - moves.sw
-        north + ne
+                |> simplify(:n, :s)
+                |> simplify(:nw, :se)
+                |> simplify(:ne, :sw)
+        sum(moves.n, moves.s, moves.se, moves.sw, moves.ne, moves.nw)
     end
+
+    def simplify(map, a, b) do
+        n1 = Map.fetch!(map, a)
+        n2 = Map.fetch!(map, b)
+        min = min(n1, n2)
+        Map.merge(map, %{a => n1 - min, b => n2 - min})
+    end
+
+    def sum(n, 0, 0, 0, ne, nw), do: n + max(ne, nw)
+    def sum(n, 0, se, 0, ne, 0), do: ne + max(n, se)
+    def sum(n, 0, 0, sw, 0, nw), do: nw + max(n, sw)
+    def sum(n, 0, se, sw, 0, 0), do: abs(n - min(se, sw)) + abs(se - sw)
+    def sum(0, s, se, sw, 0, 0), do: s + max(se, sw)
+    def sum(0, s, se, 0, ne, 0), do: se + max(s, ne)
+    def sum(0, s, 0, sw, 0, nw), do: sw + max(s, nw)
+    def sum(0, s, 0, 0, ne, nw), do: abs(s - min(ne, nw)) + abs(ne - nw)
 
     def follow_path2(input), do:
         Enum.to_list(30..Enum.count(input))
