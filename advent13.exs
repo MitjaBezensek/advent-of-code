de27bdb88a22ad5e8ba84944555f839c274a619d
Mitja Bezenšek <mitja.bezensek@gmail.com>
2017-12-13 20:08:53 +0100

    Add day 13.

---
+++ advent13.exs
@@ -0,0 +1,24 @@
+defmodule Advent13 do
+    def part1(rows) do
+        parse(rows)
+            |> Enum.filter(fn [level, range] -> is_caught?(level, range, 0) end)
+            |> Enum.map(fn [level, range] -> level * range end)
+            |> Enum.sum()
+            |> IO.inspect
+    end
+
+    def part2(rows), do: recurse(0, parse(rows)) |> IO.inspect
+
+    def recurse(delay, rows) do
+        case Enum.any?(rows, fn [level, range] -> is_caught?(level, range, delay) end) do
+            false -> delay
+            true -> recurse(delay + 1, rows)
+        end
+    end
+
+    def is_caught?(level, range, delay), do: rem(delay + level, 2 * (range - 1)) == 0
+    def parse(rows), do: rows |> Enum.map(fn [a, b] -> [String.to_integer(a), String.to_integer(b)] end)
+end
+
+File.read!("input13") |> String.trim |> String.split("\n") |> Enum.map(fn row -> String.split(row, ": ") end) |> Advent13.part1
+File.read!("input13") |> String.trim |> String.split("\n") |> Enum.map(fn row -> String.split(row, ": ") end) |> Advent13.part2
